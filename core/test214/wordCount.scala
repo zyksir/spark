57614bd4a96b7b0c112a869361e975470099db73
zyksir <jessenykzhu@tencent.com>
2021-11-24 12:58:40 -0800

    add output of WordCount RDDInfo

--- core/test214/wordCount.scala
+++ core/test214/wordCount.scala
@@ -25,16 +25,18 @@ val averageData = gradeData.
   map(line => ((line(0), line(1)), line(3).toInt)).
   mapValues((_, 1)).
   reduceByKey((x, y) => (x._1 + y._1, x._2 + y._2)).
-  mapValues(x => x._1/x._2).setName("averageData").persist()
+  mapValues(x => x._1/x._2).setName("averageData")
 println("\nStudent grade lower than 87: \n")
 averageData.filter(_._2 < 87).collect().foreach(println)
 println("\nStudent grade lower than 91: \n")
 averageData.filter(_._2 > 91).collect().foreach(println)
+println("\nStudent grade lower than 92: \n")
+averageData.filter(_._2 > 92).collect().foreach(println)
 var allRDDIno = sc.AllRDDStorageInfo()
 allRDDIno.map(x => (x.recomputeCount, x.totalUsedCount, x.name, x.parentIds, x.id))
 
 import java.io._
-val pw = new PrintWriter(new File("./core/test214/WordCountRDDInfo.txt" ))
+val pw = new PrintWriter(new File("./core/test214/WordCountRDDInfo_Nopersist.txt" ))
 allRDDIno.sortWith(_.id < _.id).
-  foreach(x => pw.write(f"${x.id}, ${x.parentIds}, ${x.name}, ${x.isCached}, ${x.recomputeCount}, ${x.totalUsedCount}\n"))
+  foreach(x => pw.write(f"${x.id}\t${x.parentIds.mkString(",")}\t${x.name}\t${x.isCached}\t${x.recomputeCount}\t${x.totalUsedCount}\n"))
 pw.close
